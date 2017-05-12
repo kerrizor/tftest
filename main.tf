a2154f3c86269b048506b60db192303e212221c1
Kerri Miller <kerrizor@kerrizor.com>
2017-05-12 10:09:05 -0700

    business

--- main.tf
+++ main.tf
@@ -1,7 +1,45 @@
+terraform {
+  backend "atlas" {
+    name         = "kerrizor/obfuscation_test"
+    access_token = "vO7GeY9GPsOJsg.atlasv1.93sHm3KnGoTyNHkDNfSgCyGgraJTznvbWtqzcXiMwPK5cE1bskq9zvR2V9N09qnOjfg"
+    address      = "https://atlas.hashicorptest.com"
+  }
+}
+
+atlas {
+  name = "kerrizor/obfuscation_test"
+}
+
+variable "sensitive" {
+    default = "foo"
+}
+
+variable "not-sensitive" {
+    default = "bar"
+}
+variable "stand-in" {
+    default = "baz"
+}
+
 resource "template_file" "hello_world" {
     template = "identity.tpl"
+}
 
-    vars {
-        input = "Hello1, World!🤖"
-    }
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+
+  byte_length = 8
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
+}
+
+output "unsensitive" {
+  value = "${var.not-sensitive}"
+}
+output "sensitive" {
+  value = "${var.sensitive}"
 }
